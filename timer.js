48c56c3a57126435642ba6c94cb5241f94812914
rudson <rudson@Predator04-Inspiron-3551.attlocal.net>
2019-03-22 18:31:48 -0400

    adding timer js and fixing structure of html

---
+++ timer.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function timer(input) {
+    
+    let divTimer = document.querySelector('timer'); 
+    let number = document.getElementById("number").value; 
+    if (isNaN(number) || number < 1 )
+    {
+        alert("Input must be a positive number"); 
+    }
+    else 
+    {
+    
+        for (let i = 0; input > i; i--) 
+        {
+
+        }
+
+    }
+}
