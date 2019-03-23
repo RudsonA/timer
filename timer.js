c5f9e969c4f6d5f168e7ef3e7f4eb7cf446d2395
rudson <rudson@Predator04-Inspiron-3551.attlocal.net>
2019-03-22 21:05:17 -0400

    needs to loop on the screen

--- timer.js
+++ timer.js
@@ -1,20 +1,18 @@
 "use strict";
 
-function timer(input) {
-    
-    let divTimer = document.querySelector('timer'); 
-    let number = document.getElementById("number").value; 
-    if (isNaN(number) || number < 1 )
-    {
-        alert("Input must be a positive number"); 
-    }
-    else 
-    {
-    
-        for (let i = 0; input > i; i--) 
-        {
+function timer() {
 
-        }
+    let divTimer = document.getElementById("timer");
+    let number = document.getElementById("number").value;
 
+    if (isNaN(number) || number < 1) {
+        alert("Input must be a positive number");
     }
+
+    for (let i = number; i > -1; i--) {
+        divTimer.innerHTML = "Countdown " + i + "...";
+        console.log(i);
+    }
+
+
 }
